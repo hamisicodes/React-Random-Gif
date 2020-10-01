2165fd1eac6a237de732e155d72b01a1938e49c8
hamisicodes <newhamisi@gmail.com>
2020-10-01 17:43:32 +0300

    declare the components in their separate folderand instantiate them in the App component 🌙

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './App.css';
+import Tag from './components/Tag_v1';
+import Random from './components/Random_v1'
 
 const App = () => (
     <div>
         <h1>Random GIF Application</h1>
+        <div className="main-container">
+            <Random/>
+            <Tag/>
+        </div>
     </div>
 );
 
